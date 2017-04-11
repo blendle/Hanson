081d69d0b04aa6f911e05024ba1d51138fb400ab
Joost van Dijk <jwvd@me.com>
2017-04-11 10:23:42 +0200

    Document Bindable.Value

--- Hanson/Observables/Bindable.swift
+++ Hanson/Observables/Bindable.swift
@@ -11,6 +11,7 @@ import Foundation
 /// Types conforming to the `Bindable` protocol can be updated with a new value when an event is published from an `EventPublisher`. Both ends of a binding should implement the `Bindable` protocol, as the receiving bindable will be set to the value of the source bindable when the binding is made.
 public protocol Bindable: class {
     
+    /// The type of value of the bindable.
     associatedtype Value
     
     /// The value of the bindable.
